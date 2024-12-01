7f3cbdba49405198c1668cdb0a1814f4f21eefca
CodingGreen <17253156+CodingGreen@users.noreply.github.com>
2024-12-01 21:49:32 +0000

    Day 1

--- src/day1.js
+++ src/day1.js
@@ -1,9 +1,68 @@
-function partOne(input) {
+const {
+  pipe,
+  split,
+  trim,
+  map,
+  transpose,
+  sortBy,
+  identity,
+  zipWith,
+  apply,
+  subtract,
+  filter,
+  converge,
+  nth,
+  multiply,
+  useWith,
+  countBy,
+  props,
+  defaultTo,
+  prop,
+  sum,
+} = require('ramda');
 
+const parseLists = pipe(
+  trim,
+  split('\n'),
+  map(pipe(split(' '), filter(identity), map(Number))),
+  transpose,
+);
+
+const absoluteDifference = pipe(subtract, Math.abs);
+
+function partOne(input) {
+  return pipe(
+    parseLists,
+    map(sortBy(identity)),
+    apply(zipWith(absoluteDifference)),
+    sum,
+  )(input);
 }
 
-function partTwo(input) {
+const convertListToOccurrenceCount = pipe(props, map(defaultTo(0)));
 
+function partTwoPointFree(input) {
+  return pipe(
+    parseLists,
+    converge(zipWith(multiply), [
+      nth(0),
+      apply(
+        useWith(convertListToOccurrenceCount, [identity, countBy(identity)]),
+      ),
+    ]),
+    sum,
+  )(input);
+}
+
+const getCount = pipe(prop, defaultTo(0));
+
+function partTwo(input) {
+  const [list1, list2] = parseLists(input);
+  const list2Counts = countBy(identity, list2);
+  return pipe(
+    map((a) => a * getCount(a, list2Counts)),
+    sum,
+  )(list1);
 }
 
 module.exports = { partOne, partTwo };
--- src/day1.test.js
+++ src/day1.test.js
@@ -1,13 +1,18 @@
 const { partOne, partTwo } = require('./day1');
 
 const testData = `
-
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
 `;
 
 test('Part One', () => {
-  expect(partOne(testData)).toBe();
+  expect(partOne(testData)).toBe(11);
 });
 
 test('Part Two', () => {
-  expect(partTwo(testData)).toBe();
+  expect(partTwo(testData)).toBe(31);
 });
