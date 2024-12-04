02d4734e57c438b133575e436dc1c0b20616a840
CodingGreen <17253156+CodingGreen@users.noreply.github.com>
2024-12-04 22:57:43 +0000

    Day 3 part 2

--- src/day3.js
+++ src/day3.js
@@ -1,11 +1,43 @@
-const { sum, map } = require('ramda');
+const { sum, map, curry, pipe, reduce, startsWith, prop } = require('ramda');
+
+const matchAll = curry((regex, str) => [...str.matchAll(regex)]);
+const instructionsMatcher = /mul\((\d+),(\d+)\)/g;
 
 function partOne(input) {
-  return sum(
-    map(([_, a, b]) => a * b, [...input.matchAll(/mul\((\d+),(\d+)\)/g)]),
-  );
+  return pipe(
+    matchAll(instructionsMatcher),
+    map(([_, a, b]) => a * b),
+    sum,
+  )(input);
 }
 
-function partTwo(input) {}
+const additionalInstructionsMatcher = /mul\((\d+),(\d+)\)|do\(\)|don't\(\)/g;
+
+function partTwo(input) {
+  return pipe(
+    matchAll(additionalInstructionsMatcher),
+    reduce(
+      (accumulator, instruction) => {
+        const [command] = instruction;
+        if (startsWith("don't", command))
+          return { ...accumulator, enabled: false };
+        if (startsWith('do', command)) return { ...accumulator, enabled: true };
+        if (accumulator.enabled)
+          return {
+            ...accumulator,
+            enabledInstructions: [
+              ...accumulator.enabledInstructions,
+              instruction,
+            ],
+          };
+        return accumulator;
+      },
+      { enabled: true, enabledInstructions: [] },
+    ),
+    prop('enabledInstructions'),
+    map(([_, a, b]) => a * b),
+    sum,
+  )(input);
+}
 
 module.exports = { partOne, partTwo };
--- src/day3.test.js
+++ src/day3.test.js
@@ -1,11 +1,13 @@
 const { partOne, partTwo } = require('./day3');
 
-const testData = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
+const partOneTestData = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
 
 test('Part One', () => {
-  expect(partOne(testData)).toBe(161);
+  expect(partOne(partOneTestData)).toBe(161);
 });
 
+const partTwoTestData = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`;
+
 test('Part Two', () => {
-  expect(partTwo(testData)).toBe();
+  expect(partTwo(partTwoTestData)).toBe(48);
 });
