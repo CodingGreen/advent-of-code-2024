60d53cc02060481863c8160695103ce792c172b4
CodingGreen <17253156+CodingGreen@users.noreply.github.com>
2024-12-04 09:12:06 +0000

    day 3 part 1

--- src/day3.js
+++ src/day3.js
@@ -1,9 +1,11 @@
+const { sum, map } = require('ramda');
+
 function partOne(input) {
-
+  return sum(
+    map(([_, a, b]) => a * b, [...input.matchAll(/mul\((\d+),(\d+)\)/g)]),
+  );
 }
 
-function partTwo(input) {
-
-}
+function partTwo(input) {}
 
 module.exports = { partOne, partTwo };
--- src/day3.test.js
+++ src/day3.test.js
@@ -1,11 +1,9 @@
 const { partOne, partTwo } = require('./day3');
 
-const testData = `
-
-`;
+const testData = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
 
 test('Part One', () => {
-  expect(partOne(testData)).toBe();
+  expect(partOne(testData)).toBe(161);
 });
 
 test('Part Two', () => {
